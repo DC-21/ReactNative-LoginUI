bc61efa9cf182f0cba64d2e831158d0e3bf1f343
DC-21 <cholahkuboko@gmail.com>
2023-05-11 16:10:22 +0200

    Commit message

--- components/Home.js
+++ components/Home.js
@@ -4,16 +4,18 @@ import Background from './Background'
 import Button from './Button'
 import { darkGreen } from './Constants';
 
-const Home = () => {
-  return (
-    <Background>
-        <View style={{ marginHorizontal: 60, marginVertical: 40}}>
-        <Text style={{ color: 'white', fontSize: 64}}>Welcome</Text>
-        </View>
-        <Button bgColor={darkGreen} btnLabel='Log In' textColor='white'  press={() =>  { console.log('Button pressed'); }} />
-    </Background>
-  );
-}
+const Home = (props) => {
+    return (
+      <Background>
+          <View style={{ marginHorizontal: 40, marginVertical: 100 }}>
+          <Text style={{ color: 'white', fontSize: 64}}>Welcome</Text>
+          <Button bgColor={darkGreen} btnLabel='Log In' textColor='white'  press={() =>  props.navigation.navigate("Log In")} />
+          <Button bgColor= 'white' btnLabel='Sign Up' textColor={darkGreen}  press={() =>  props.navigation.navigate("Sign Up")} />
+          </View>
+      </Background>
+    );
+  }
+  
 
 const styles = StyleSheet.create({})
 export default Home
